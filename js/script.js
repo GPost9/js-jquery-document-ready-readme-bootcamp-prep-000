080db30c5ce9723f1a83f0c4d85fd732dc36df63
Gabriel Postelnicu <klavier_gabriel@yahoo.com>
2019-03-24 15:36:03 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
   $("#text").append("This is so freaking cool.");
-  $("#text").append(" No it isn't. I have no life.");
+  $("#text").append("No it isn't. I have no life.");
 });
